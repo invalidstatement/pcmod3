b49c41006ab569aeca33fab4b182719d775b7231
invalidstatement <legion@fortwarsrp.net>
2014-05-03 12:05:38 +0200

    Added init file

---
+++ lua/autorun/sh_init.lua
@@ -0,0 +1,13 @@
+--[[
+	© 2014 invalidstatement.eu do not share, re-distribute or modify
+	without permission of its author (teamdarker@gmail.com).
+ 
+	PCMod 3 was created by David Harris (also known as legion.)
+	https://creativecommons.org/licenses/by-nc-nd/3.0/legalcode
+--]]
+local include, AddCSLuaFile, util, MsgN = include, AddCSLuaFile, util, MsgN
+
+if SERVER then
+	AddCSLuaFile()
+elseif CLIENT then
+end
